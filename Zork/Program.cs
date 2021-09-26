d9a181505bc5bd55e1ff635c171a4889c5a56e1d
cgrave314 <59934219+cgrave314@users.noreply.github.com>
2021-09-26 15:30:17 -0400

    Zork 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Zork
 {
-    class Program
+    internal class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -14,82 +16,105 @@ namespace Zork
 
         static void Main(string[] args)
         {
+            InitializeRoomDescriptions();
+
             Console.WriteLine("Welcome to Zork!");
 
-            while (true)
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
             {
-                Console.Write($"{CurrentRoom}\n> ");
-                Commands command = ToCommand(Console.ReadLine().Trim());
-                if (command == Commands.QUIT)
-                {
-                    break;
-                }
-
-                string outputString;
+                Console.Write($"{CurrentRoom.Name}\n> ");
+                command = ToCommand(Console.ReadLine().Trim());
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing.";
+                        Console.WriteLine("Thank you for playing.");
                         break;
+
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = Move(command) ? $"You moved {command}." : "The way is shut!";
+                        if (Move(command) == false)
+                        {
+                            Console.WriteLine("The way is shut!");
+                        }
                         break;
-             
+
                     default:
-                        outputString = "Unrecognized command.";
+                        Console.WriteLine("Unknown command.");
                         break;
                 }
-
-                Console.WriteLine(outputString);
             }
-
-            Console.WriteLine("Thank you for playing!");
         }
+
+
 
         private static bool Move(Commands command)
         {
-            bool didMove = false;
+            Assert.IsTrue(Directions.Contains(command), "Invalid direction.");
 
+            bool isValidMove = true;
             switch (command)
             {
                 case Commands.NORTH when Location.row < Rooms.GetLength(0) - 1:
                     Location.row++;
-                    didMove = true;
                     break;
 
                 case Commands.SOUTH when Location.row > 0:
                     Location.row--;
-                    didMove = true;
                     break;
 
-                case Commands.EAST when Location.column < Rooms.Length - 1:
+                case Commands.EAST when Location.column < Rooms.GetLength(1) - 1:
                     Location.column++;
-                    didMove = true;
                     break;
 
                 case Commands.WEST when Location.column > 0:
                     Location.column--;
-                    didMove = true;
+                    break;
+
+                default:
+                    isValidMove = false;
                     break;
             }
 
-            return didMove;
+            return isValidMove;
         }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
-        private static readonly string[,] Rooms =
+        public static Commands ToCommand(string commandString) => Enum.TryParse(commandString, ignoreCase: true, out Commands result) ? result : Commands.UNKNOWN;
+        private static void InitializeRoomDescriptions()
         {
-            {"Rocky Trail", "South of House", "Canyon View" },
-            {"Forest", "West of House", "Behind House" },
-            {"Dense Woods", "North of House", "Clearing" }
+            Rooms[0, 0].Description = "You are on a rock-strewn trail";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barracaded.";
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded fron door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slight";
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. there is no door here, and all the windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+
+        }
+
+        private static readonly Room[,] Rooms =
+        {
+            {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
+        };
+
+        private static readonly Commands[] Directions =
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
         };
 
         private static (int row, int column) Location = (1, 1);
